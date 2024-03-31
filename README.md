36e9fe2e3adbb5171875e438ca8090234c3f485e
Charles Herring <charles@witfoo.com>
2024-03-31 15:56:34 -0500

    MVP of features

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# plex-media-inspector
-Project to find problems with media in a Plex Media Server
+# Fish's Plex Media Inspector
+My personal Plex server has more than 60,000 pieces of content. I am very careful in how I import data. Sometimes things go wrong and I need a way to do a deep inspection of the massive haystack. This Python code is written and tested on Window 11. 
+
+## Current Inspection
+The code currently checks Plex Library content for the following issues:
+- **Missing credits**. This can be a symptom of a rip of content that terminated before it should have.
+- **Missing bitrate**. If the bitrate is not detected by Plex, it's probably broken.
+- **Corrupt video (very slow)**. I included `ffmpeg` for Windows 11 in this repo. If you enable that setting, it will do a full scan of the media file to detect any encoding errors.
+
+## Set up
+1) Click on Login to Plex
+2) Give it your Plex Username
+3) Give it your Plex Password. NOTE: If you have MFA enabled, add the 6 digit code to the end of your password.
+The steps above will generate and store a token in the `settings.yaml` file for the current and future sessions so you shouldn't need to log in again (anytime soon.)
+4) Under `Settings` select which scan features you want to enable
+5) Click `Start Scan`
+
+The results will dump to the UI and to `results.csv`
+
+# Prototype Status
+This code gets the job done but needs a lot of work to be usable by many users. If anyone is interested in contributing to the repo (or forking it off), please hit me up.
+
+![Logo](logo.jpg)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,359 @@
+import csv
+import tkinter as tk
+from tkinter import ttk, Menu, simpledialog
+import tkinter.messagebox as messagebox
+import time
+import yaml
+import os
+import subprocess
+from plexapi.myplex import MyPlexAccount
+from plexapi.exceptions import Unauthorized
+from plexapi.server import PlexServer
+import threading
+
+BITRATE_MINIMUM = 1000
+
+def get_libraries():
+    global plex_api
+    return plex_api.library.sections()
+
+# Width of the Screen
+window_width = 1024
+window_height = 780
+total_width = window_width - 100
+
+root = tk.Tk()
+root.title("Fish's Plex Media Inspector")
+root.geometry(str(window_width) + "x" + str(window_height))  
+
+frame = tk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=1, padx=50, pady=50)
+
+def get_library_item_count():
+    global plex_api
+    library = plex_api.library
+    return len(library.all())
+
+scanning = False
+scan_thread = None
+
+def start_scan():
+    global library_total_item_count
+    global library_items_scanned
+    global per_item_scan_seconds
+    global total_scan_seconds
+    global plex_api
+    global selected_library
+    global scanning
+    global scan_thread
+    library_total_item_count = 0
+    library_items_scanned = 0
+    per_item_scan_seconds = 1
+    total_scan_seconds = 0
+    remaining_seconds = per_item_scan_seconds * library_total_item_count
+    remaining_time = time.strftime('%H:%M:%S', time.gmtime(remaining_seconds))
+    update_progress()
+    if not scanning:
+        scanning = True
+        library_menu.config(state="disabled")
+        start_button.config(text="Abort Scan")
+        library_total_item_count = get_library_item_count()
+        update_progress()
+        scan_thread = threading.Thread(target=scan_library_meta, args=(plex_api, selected_library.get()))
+        scan_thread.start()
+    else:
+        scanning = False
+        library_menu.config(state="enabled")
+        start_button.config(text="Start Scan")
+        if scan_thread is not None:
+            scan_thread.join()  # Wait for the thread to finish
+    
+def scan_library_meta(plex, library_name):
+    global library_items_scanned
+    global total_scan_seconds
+    global library_total_item_count
+    global scanning
+    global writer
+    # Create a variable for the CSV file
+    csv_file = 'results.csv'
+
+    # Create a writer object for the CSV file
+    writer = csv.writer(open(csv_file, 'w', newline=''))
+
+    # Write the header row to the CSV file
+    writer.writerow(['Library', 'Title', 'Plex Link', 'Disk Link', 'Problems'])
+
+    # Get the library by name
+    library = plex.library.section(library_name)
+
+    # Create an empty list to store content with issues
+    problem_content = []
+    start_time = time.time()
+    # Iterate over all items in the library
+    for item in library.all():
+        # Check if scanning should be aborted
+        if not scanning:
+            writer.close()
+            break
+        # Skip items that are not movies or shows
+        if item.type not in ['movie', 'show']:
+            library_items_scanned += 1
+            update_progress()
+            continue
+        if not hasattr(item, 'hasCreditsMarker'):
+            library_items_scanned += 1
+            update_progress()
+            continue
+        problems = []
+        # Check if the item has detected credits
+        if not item.hasCreditsMarker and check_credits.get():
+            if item.enableCreditsMarkerGeneration == 0:
+                item.enableCreditsMarkerGeneration = 1
+            else:
+                problems.append("No credits detected")
+        # Check if the item has a valid bitrate
+        bitrate = item.media[0].bitrate
+        if bitrate <= BITRATE_MINIMUM and validate_bitrate.get():
+            problems.append("Invalid bitrate")
+        if check_corrupt_video:
+            corrupt = check_corrupt_video(item.media[0].parts[0].file)
+        if corrupt:
+            problems.append("Corrupt video file detected")
+        if problems:
+            print("Problems found with " + item.title)
+            add_result(library_name, item.title, item.guid, item.media[0].parts[0].file, problems)
+        library_items_scanned += 1
+        total_scan_seconds = time.time() - start_time
+        update_progress()
+    # Return the list of content without credits
+    library_items_scanned = library_total_item_count
+    update_progress()
+    writer.close()
+
+ 
+# Status variables
+library_total_item_count = 0
+library_items_scanned = 0
+per_item_scan_seconds = 1
+total_scan_seconds = 0
+remaining_seconds = per_item_scan_seconds * library_total_item_count
+remaining_time = time.strftime('%H:%M:%S', time.gmtime(remaining_seconds))
+plex_connected = False
+results = []
+
+def add_result(library, title, plex_link, disk_link, problems):
+    global writer
+    # Determine the tag based on the number of existing children
+    tag = "oddrow" if len(results_table.get_children()) % 2 == 0 else "evenrow"
+    # Insert the result with the determined tag
+    results_table.insert('', 'end', values=(library, title, plex_link, disk_link, problems), tags=(tag))
+    # Write the result to the CSV file
+    writer.writerow([library, title, plex_link, disk_link, problems])
+
+# Create variables for settings options and set them to True
+check_credits = tk.BooleanVar(value=True)
+validate_bitrate = tk.BooleanVar(value=True)
+check_encoding_errors = tk.BooleanVar(value=True)
+
+# Create variables for Plex settings
+plex_account_name = tk.StringVar()
+plex_password = tk.StringVar()
+plex_token = tk.StringVar()
+plex_server = tk.StringVar()
+plex_server.set('http://127.0.0.1:32400')
+plex_api = None
+
+
+# Function to test the Plex connection
+def test_plex_connection():
+    global plex_connected
+    global plex_api
+    try:
+        plex_api = PlexServer(plex_server.get(), plex_token.get())
+        plex_connected = True
+    except Unauthorized:
+        plex_connected = False
+        try:
+            connection_status.config(text="Plex Connection: Unauthorized", fg="red")
+            connection_indicator.itemconfig(connection_circle, fill="red")
+            start_button.config(state=tk.DISABLED)
+            library_menu.config(state="disabled")
+            return False
+        except Exception as e:
+            return False
+    if plex_connected:
+        try:
+            connection_status.config(text="Plex Connection: Connected", fg="green")
+            connection_indicator.itemconfig(connection_circle, fill="green")
+            library_menu.config(state="normal")
+            return True
+        except Exception as e:
+            return True
+# Function to update the progress bar and label
+def update_progress():
+    global library_total_item_count
+    global per_item_scan_seconds
+    global remaining_seconds
+    global remaining_time
+    global library_items_scanned
+    global progress_label
+    global progress
+    if library_total_item_count > 0:
+        progress_value = library_items_scanned / library_total_item_count
+    else:
+        progress_value = 0
+    if library_items_scanned > 0:
+        per_item_scan_seconds = total_scan_seconds / library_items_scanned
+    else:
+        per_item_scan_seconds = 0
+    remaining_seconds = per_item_scan_seconds * (library_total_item_count - library_items_scanned)
+    remaining_time = time.strftime('%H:%M:%S', time.gmtime(remaining_seconds))
+    progress_label.config(text="Progress: " + str(library_items_scanned) + " / " + str(library_total_item_count) + " items scanned. Estimated time remaining: " + remaining_time + ". ")
+    progress['value'] = progress_value * 100
+    root.update_idletasks()
+
+def save_settings():
+    settings = {
+        'plex_account_name': plex_account_name.get(),
+        'plex_token': plex_token.get(),
+        'plex_server': plex_server.get(),
+        'check_credits': check_credits.get(),
+        'validate_bitrate': validate_bitrate.get(),
+        'check_encoding_errors': check_encoding_errors.get(),
+    }
+    with open('settings.yaml', 'w') as file:
+        yaml.dump(settings, file)
+
+
+def load_settings():
+    if not os.path.exists('settings.yaml'):
+        save_settings()
+    with open('settings.yaml', 'r') as file:
+        settings = yaml.safe_load(file)
+    plex_account_name.set(settings['plex_account_name'])
+    plex_token.set(settings['plex_token'])
+    plex_server.set(settings['plex_server'])
+    check_credits.set(settings['check_credits'])
+    validate_bitrate.set(settings['validate_bitrate'])
+    check_encoding_errors.set(settings['check_encoding_errors'])
+
+
+# Load settings when the program starts
+load_settings()
+
+
+# Create a frame for the connection indicator and status
+connection_frame = tk.Frame(frame)
+connection_frame.pack(pady=10)
+
+# Create a canvas for the connection indicator inside the connection frame
+connection_indicator = tk.Canvas(connection_frame, width=20, height=20)
+connection_circle = connection_indicator.create_oval(5, 5, 15, 15, fill="red")
+connection_indicator.pack(side=tk.LEFT)
+
+# Create a label for the connection status inside the connection frame
+connection_status = tk.Label(connection_frame, text="Plex Connection: Unknown", fg="red")
+connection_status.pack(side=tk.LEFT)
+
+# Get an authentication token from Plex
+def plex_login():
+   # Create a form with Plex Username and Password
+    plex_account_name.set(simpledialog.askstring("Plex Login", "Enter your Plex username"))
+    plex_password.set(simpledialog.askstring("Plex Login", "Enter your Plex password", show="*"))
+    try:
+        account = MyPlexAccount(plex_account_name.get(), plex_password.get())
+        plex_token.set(account.authenticationToken)
+    except Unauthorized:
+        messagebox.showerror("Plex Login", "Invalid username or password")
+        return
+    save_settings()
+    test_plex_connection()
+
+# Create a "Test Connection" button
+test_connection_button = tk.Button(frame, text="Plex Login", command=plex_login)
+test_connection_button.pack(pady=10)
+
+# Create a label for the progress
+progress_label = tk.Label(frame, text="Progress: 0 / 0")
+progress_label.pack(pady=5)
+
+# Add the progress bar, start button, and menu bar below the new widgets
+progress = ttk.Progressbar(frame, length=100, mode='determinate')
+progress.pack(fill=tk.X, expand=1, pady=5)
+
+start_button = tk.Button(frame, text="Start Scan", command=start_scan)
+if not plex_connected:
+    start_button.config(state=tk.DISABLED)
+start_button.pack(pady=10)
+
+# Create a variable for the selected library
+selected_library = tk.StringVar()
+
+# Create a label for the library dropdown menu
+library_label = tk.Label(frame, text="Library")
+library_label.pack(pady=5)
+
+# Create a dropdown menu of Plex libraries
+libraries = []
+if test_plex_connection():
+    libraries = get_libraries()
+library_menu = ttk.Combobox(frame, textvariable=selected_library)
+library_menu['values'] = [library.title for library in libraries]
+library_menu.pack(pady=10)
+def on_library_select(event):
+    start_button.config(state=tk.NORMAL)
+
+library_menu.bind("<<ComboboxSelected>>", on_library_select)
+if not plex_connected:
+    library_menu.config(state="disabled")
+
+# Create a frame for the Results group
+results_frame = tk.LabelFrame(frame, text="Results", height=600)
+results_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+# Create a table for the Results group
+results_table = ttk.Treeview(results_frame, columns=("Library", "Title", "Plex Link", "Disk Link", "Problems"), show="headings")
+results_table.heading("Library", text="Library")
+results_table.heading("Title", text="Title")
+results_table.heading("Plex Link", text="Plex Location")
+results_table.heading("Disk Link", text="Disk Location")
+results_table.heading("Problems", text="Problems")
+# Set the width of each column
+results_table.column("Library", width=int(total_width * 0.10))
+results_table.column("Title", width=int(total_width * 0.25))
+results_table.column("Plex Link", width=int(total_width * 0.10))
+results_table.column("Disk Link", width=int(total_width * 0.10))
+results_table.column("Problems", width=int(total_width * 0.45))
+results_table.pack(fill=tk.BOTH, expand=True)
+# Create two tags with different background colors
+results_table.tag_configure("oddrow", background="white")
+results_table.tag_configure("evenrow", background="light gray")
+
+# Add a scrollbar
+scrollbar = ttk.Scrollbar(results_frame, orient="vertical", command=results_table.yview)
+scrollbar.pack(side="right", fill="y")
+results_table.configure(yscrollcommand=scrollbar.set)
+
+# Create a menu bar
+menubar = Menu(root)
+root.config(menu=menubar)
+# Create a settings menu
+settings_menu = Menu(menubar, tearoff=0)
+
+# Add settings menu to the menu bar
+menubar.add_cascade(label="Settings", menu=settings_menu)
+
+# Add settings options to the settings menu
+settings_menu.add_checkbutton(label="Check for Credits", onvalue=True, offvalue=False, variable=check_credits)
+settings_menu.add_checkbutton(label="Validate Bitrate", onvalue=True, offvalue=False, variable=validate_bitrate)
+settings_menu.add_checkbutton(label="Check for encoding errors (very slow)", onvalue=True, offvalue=False, variable=check_encoding_errors)
+
+def check_corrupt_video(filepath):
+    try:
+        # Run ffmpeg command to check if the video file is corrupt
+        subprocess.check_output(['ffmpeg', '-v', 'error', '-i', filepath, '-f', 'null', '-'], stderr=subprocess.STDOUT)
+        return False  # Video file is not corrupt
+    except subprocess.CalledProcessError as e:
+        return True  # Video file is corrupt
+    
+root.mainloop()
